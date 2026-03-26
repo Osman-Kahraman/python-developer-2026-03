d485ec0b1112d564fa67e71e174038b98a1e0f0e
Osman Kahraman <o_kahraman@outlook.com>
2026-03-26 15:20:52 -0400

    fix: ruff formatting

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,28 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+
 from .models import Post
+
 
 def index(request: HttpRequest) -> HttpResponse:
     # Load all published posts in reverse chronological order
-    posts = Post.objects.filter(published_at__lte = timezone.now()).order_by('-published_at').select_related('author')
+    posts = (
+        Post.objects.filter(published_at__lte=timezone.now())
+        .order_by("-published_at")
+        .select_related("author")
+    )
     return render(request, "blog/index.html", {"posts": posts})
+
 
 def welcome(request: HttpRequest) -> HttpResponse:
     return render(request, "blog/welcome.html")
 
+
 def post_detail(request: HttpRequest, post_id: str) -> HttpResponse:
     post = get_object_or_404(
-        Post.objects.select_related('author'),
+        Post.objects.select_related("author"),
         id=post_id,
-        published_at__lte=timezone.now()
+        published_at__lte=timezone.now(),
     )
     return render(request, "blog/post_detail.html", {"post": post})
